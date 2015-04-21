c21ed24b828a423a950a767160f06c7f90224153
paulbible <paulbible@pauls-air.home>
2015-04-20 22:36:54 -0400

    edits

--- src/incanter_activity/core.clj
+++ src/incanter_activity/core.clj
@@ -8,32 +8,41 @@
 
 (use '(incanter core io charts stats datasets optimize))
 
+;; Getting started
+;;
+;; git clone https://github.com/paulbible/incanter-activity
+;; cd incanter-activity
+;; lein repl
+;; (ns incanter-activity.core)
+
+
+;;;; Activity ;;;;;
+;; 1) Plot a moving average for period 15 and period 30.
+;;
+;; 2) Find the intersection points for these movinging averages
+;;
+;; 3) Find local minima of the chart
+;;
+;; 4) plot a trend line
+
+
 (def quotes-fname "data/HistoricalQuotes_T.csv")
 
 (def quote-data (read-dataset quotes-fname
                               :delim \,
                               :header true))
 
-;;;; Activity ;;;;;
-;; Plot a moving average for period 15 and period 30.
-;;
-;; 1) Find the intersection points for these movinging averages
-;;
-;; 2) Find local minima of the chart
-;;
-;; 3) plot a trend line
-
-
-;; getting started, a method for moving average
+;; a method for moving average
 (map mean (partition 5 1 ($ :close quote-data)))
 
 ;; a plot with moving average a an example verticle line
-(with-data quote-data
-  (doto (xy-plot (range 0 (nrow $data)) ($ :close $data))    
-    (set-y-range (reduce min ($ :low quote-data))
-                 (reduce max ($ :high quote-data)))
-    (add-lines (range 15 (nrow $data))
-               (map mean (partition 15 1 ($ :close quote-data))))
-    (add-lines (list 50 50) (list 0 50))
-    (view)))
+(defn show []
+  (with-data quote-data
+    (doto (xy-plot (range 0 (nrow $data)) ($ :close $data))    
+      (set-y-range (reduce min ($ :low quote-data))
+                   (reduce max ($ :high quote-data)))
+      (add-lines (range 15 (nrow $data))
+                 (map mean (partition 15 1 ($ :close quote-data))))
+      (add-lines (list 50 50) (list 0 50))
+      (view))))
 
